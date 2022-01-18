2f5814a72428faa15bc99eedad0b957f06eb1d55
sinatriobimo <sinatrio.bimo@mekari.com>
2022-01-18 08:57:02 +0700

    feat(master): finishing

--- app.js
+++ app.js
@@ -40,8 +40,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/sb-admin-2', express.static(path.join(__dirname, 'node_modules/startbootstrap-sb-admin-2')));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET');
+  res.header('Access-Control-Allow-Origin', 'http://admin-mapres.herokuapp.com');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 })
