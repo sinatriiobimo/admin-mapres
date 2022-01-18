09f9473ca5d2d2bd799f60c28e0277d2b8555ad6
sinatriobimo <sinatrio.bimo@mekari.com>
2022-01-18 08:33:46 +0700

    feat(master): finishing

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use('/sb-admin-2', express.static(path.join(__dirname, 'node_modules/startbo
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 })
 
